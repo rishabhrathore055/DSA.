fe5bd3ab2e319ee5f268d94747aba02be056cdcb
rishu <rishabhrathore055@gmail.com>
2023-08-20 01:02:14 +0530

    Length of longest subarray

---
+++ Arrays/length_of_longest_subarray.py
@@ -0,0 +1,24 @@
+def longestSubarry( nums):
+    res = 0
+    count = 0
+    for n in nums:
+        if n>=0:
+            count+=1
+        else:
+            count = 0
+        res = max(count,res)
+    return count
+
+def longestSubarry( nums, N):
+    res = 0
+    count = 0
+    for n in nums:
+        if n<0:
+            count = 0
+        else:
+            count+=1
+        res = max(count,res)
+    return res
+    
+    
+print(longestSubarry([1,-2,3,2,12,-1,2]))
