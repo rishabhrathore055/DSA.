d588dd7a05c7e4994cb4dd10d22c5a4aa6d38ea8
rishu <rishabhrathore055@gmail.com>
2023-09-03 21:06:05 +0530

    Check if a strings is subsequence of other

---
+++ Strings/check_if_a_subsequence_of_other.py
@@ -0,0 +1,24 @@
+# itrative solutions
+def isSubsequence(s1,s2):
+    n = len(s1)
+    m = len(s2)
+    i,j = 0 , 0
+
+    while i!=n and j!=m:
+        if (s1[i] == s2[j]):
+            j+=1
+        i+=1
+    return j == m
+print(isSubsequence("ABCD","AD"))
+
+
+# recursion solution
+def isSubsequenceRec(s,t,n,m):
+    if m == 0 :return True
+    if n == 0 :return False
+    if (s[n-1]==t[m-1]):
+        return isSubsequenceRec(s,t,n-1,m-1)
+    else:
+        return isSubsequenceRec(s,t,n-1,m)
+
+print(isSubsequenceRec("ABCD","AD",4,2))
