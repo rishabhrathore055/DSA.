04766655bc5b38112f8675d26971bb24b964bb80
rishu <rishabhrathore055@gmail.com>
2023-09-06 20:25:26 +0530

    11 pattern done

---
+++ Patterns/11.py
@@ -0,0 +1,18 @@
+"""
+1
+0 1
+1 0 1
+0 1 0 1
+1 0 1 0 1
+0 1 0 1 0 1
+
+"""
+n = int(input("Enter the value of n : "))
+start = 1
+for i in range(n):
+    if(i%2==0): start = 1
+    else : start = 0
+    for j in range(i+1):
+        print(start,end=" ")
+        start = 1 - start
+    print()
