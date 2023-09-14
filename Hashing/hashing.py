f40175160f56471685963f075e93df1dad95225a
rishu <rishabhrathore055@gmail.com>
2023-09-14 23:41:46 +0530

    .

--- Hashing/hashing.py
+++ Hashing/hashing.py
@@ -1,11 +1,11 @@
-# word = dict()
-# word['Web Dev'] = 1
-# word['Andriod'] = 1
-# word['Data Science'] = 1
-# word['Web Dev'] = word['Web Dev'] + 1
-# # print(word['Andriod'])
-# counts = dict()
-# domain = ['Web Dev','Andriod','Data Science']
-# for jobs in domain:
-#     counts[jobs] = counts.get(jobs,0) + 1
-# print(counts)
+word = dict()
+word['Web Dev'] = 1
+word['Andriod'] = 1
+word['Data Science'] = 1
+word['Web Dev'] = word['Web Dev'] + 1
+print(word['Andriod'])
+counts = dict()
+domain = ['Web Dev','Andriod','Data Science']
+for jobs in domain:
+    counts[jobs] = counts.get(jobs,0) + 1
+print(counts)
