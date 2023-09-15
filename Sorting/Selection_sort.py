a56c5dc9550ccd68fe4a4574e13f1f6109a77fe3
rishu <rishabhrathore055@gmail.com>
2023-09-15 22:16:34 +0530

    Selection sort

---
+++ Sorting/Selection_sort.py
@@ -0,0 +1,17 @@
+# get the minimum and sort it
+def selection_sort(nums,n): # O(n^2)
+    def Swap(nums,a,b):
+        nums[a],nums[b] = nums[b],nums[a]
+        return nums
+    for i in range(n-1):
+        mini = i
+        for j in range(i,n):
+            if nums[j]  < nums[mini]:
+                mini = j
+        Swap(nums,mini,i)
+    return nums
+print(selection_sort([113,13,12,32,13],5))
+
+
+
+     
---
+++ Sorting/readme.md
@@ -0,0 +1,2 @@
+### Sorting alogs
+[1. Selection sort](Selection_sort.py)
